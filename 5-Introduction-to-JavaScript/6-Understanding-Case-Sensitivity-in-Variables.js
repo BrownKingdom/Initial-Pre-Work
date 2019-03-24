b7ccc711130932b1d6a406f63a4a6407a46570eb
BrownKingdom <35633631+BrownKingdom@users.noreply.github.com>
2019-03-24 14:46:46 -0600

    Update 6-Understanding-Case-Sensitivity-in-Variables.js

---
+++ 5-Introduction-to-JavaScript/6-Understanding-Case-Sensitivity-in-Variables.js
@@ -0,0 +1,10 @@
+<!-- variable may have the same name but I must be very careful in where or how I put in capitalization this is case sensitive.-->
+// Declarations
+var studlyCapVar;
+var properCamelCase;
+var titleCaseOver;
+
+// Assignments
+studlyCapVar = 10;
+properCamelCase = "A String";
+titleCaseOver = 9000;
