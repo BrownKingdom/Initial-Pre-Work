6668bad2d43a831e2a9beb9976a0c13eb7a2c247
BrownKingdom <35633631+BrownKingdom@users.noreply.github.com>
2019-03-24 14:56:10 -0600

    Update 5-Understanding-Uninitialized-Variables.js

---
+++ 5-Introduction-to-JavaScript/5-Understanding-Uninitialized-Variables.js
@@ -0,0 +1,15 @@
+<!-- With declared javascript variables they have an initial value of undefined. If I run a mathematical operation I may get NaN as my result. -->
+// Initialize these three variables
+var a;
+var b;
+var c;
+
+// Do not change code below this line
+
+a = a + 1;
+b = b + 5;
+c = c + " String!";
+
+var a = 6;
+var b = 15;
+var c = "I am a String!";
