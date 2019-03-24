eebb49ced48a5bc4873adb9c9f0603346f4a722f
BrownKingdom <35633631+BrownKingdom@users.noreply.github.com>
2019-03-24 15:31:38 -0600

    Update 13-Create-Decimal-Numbers-with-JavaScript.js

---
+++ 5-Introduction-to-JavaScript/13-Create-Decimal-Numbers-with-JavaScript.js
@@ -0,0 +1,5 @@
+var ourDecimal = 5.7;
+
+// Only change code below this line
+var myDecimal = 1.234
+<!-- using the myDecimal on the variable line with and equal sign and a number in decimal form -->
