eafe7de02ea782d4c1e6d4406cf49c85ecab2ae8
BrownKingdom <35633631+BrownKingdom@users.noreply.github.com>
2019-03-25 06:27:30 -0600

    Update 29-Find_the_Length_of_a_String.js

---
+++ 5-Introduction-to-JavaScript/29-Find_the_Length_of_a_String.js
@@ -0,0 +1,11 @@
+<!-- using the += operator to append variable strings on the myStr line with the += operator do not use quotes -->
+// Example
+var anAdjective = "awesome!";
+var ourStr = "freeCodeCamp is ";
+ourStr += anAdjective;
+
+// Only change code below this line
+
+var someAdjective = "Lovely";
+var myStr = "Learning to code is ";
+myStr += someAdjective;
