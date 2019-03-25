50b0e6aafaa321d6311103e08f38aeaa238cd2a4
BrownKingdom <35633631+BrownKingdom@users.noreply.github.com>
2019-03-25 17:06:42 -0600

    Update 72-Golf_Code.js

---
+++ 5-Introduction-to-JavaScript/72-Golf_Code.js
@@ -0,0 +1,32 @@
+<!-- My function will pass through par and stroke arguments to render the correct string according to determined values in a table. -->
+var names = ["Hole-in-one!", "Eagle", "Birdie", "Par", "Bogey", "Double Bogey", "Go Home!"];
+function golfScore(par, strokes) {
+  // Only change code below this line
+  if (strokes === 1) {
+    return "Hole-in-one!";
+  }
+  else if (strokes <= (par -2)) {
+    return "Eagle";
+  }
+  else if (strokes === (par - 1)) {
+    return "Birdie";
+  }
+  else if (strokes === par) {
+    return "Par";
+  }
+  else if (strokes === (par + 1)) {
+    return "Bogey";
+  }
+  else if (strokes === (par + 2)) {
+    return "Double Bogey";
+  }
+  else if (strokes >= (par +3)) {
+    return "Go Home!";
+  }
+  
+  return "Change Me";
+  // Only change code above this line
+}
+
+// Change these values to test
+golfScore(6, 5);
