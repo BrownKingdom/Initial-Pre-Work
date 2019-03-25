54d7853805a07827ddd88fbeb4072338d3f39ae2
BrownKingdom <35633631+BrownKingdom@users.noreply.github.com>
2019-03-25 14:51:55 -0600

    Update 54-Stand_in_Line.js

---
+++ 5-Introduction-to-JavaScript/54-Stand_in_Line.js
@@ -0,0 +1,17 @@
+<!-- the queue function pushes and removes items inline with an arr variable function. -->
+function nextInLine(arr, item) {
+  // Your code here
+  arr.push(item);
+  var removed = arr.shift();
+
+
+  return removed;  // Change this line
+}
+
+// Test Setup
+var testArr = [1,2,3,4,5];
+
+// Display Code
+console.log("Before: " + JSON.stringify(testArr));
+console.log(nextInLine(testArr, 6)); // Modify this line to test
+console.log("After: " + JSON.stringify(testArr));
