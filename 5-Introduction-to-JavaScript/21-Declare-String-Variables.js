73c6e616b8a1b6bfbde3e411779f070a58b2eea5
BrownKingdom <35633631+BrownKingdom@users.noreply.github.com>
2019-03-24 18:44:06 -0600

    Update 21-Declare-String-Variables.js

---
+++ 5-Introduction-to-JavaScript/21-Declare-String-Variables.js
@@ -0,0 +1,8 @@
+<!-- adding the name fields with tw new string variables -->
+// Example
+var firstName = "Alan";
+var lastName = "Turing";
+
+// Only change code below this line
+var myFirstName = "Yolanda";
+var myLastName = "Honeycutt-Brown";
