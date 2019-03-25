3e16e6a3a9cbf810cecc36cd18e0eb478af287f3
BrownKingdom <35633631+BrownKingdom@users.noreply.github.com>
2019-03-25 15:19:37 -0600

    Update 57-Comparison_with_the_Equality_Operator.js

---
+++ 5-Introduction-to-JavaScript/57-Comparison_with_the_Equality_Operator.js
@@ -0,0 +1,11 @@
+<!-- Comparison operators in js render a true or false statement.  Using the double equal compares two values and rtn t/f statements. Make adjustments to  myVal or val element.-->
+// Setup
+function testEqual(val) {
+  if (val == 12) { // Change this line
+    return "Equal";
+  }
+  return "Not Equal";
+}
+
+// Change this value to test
+testEqual(10);
