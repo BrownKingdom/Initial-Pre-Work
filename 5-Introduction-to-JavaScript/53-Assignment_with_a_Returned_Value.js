8f7f24ce901b75b0a18ccd31013fcdaaae23712f
BrownKingdom <35633631+BrownKingdom@users.noreply.github.com>
2019-03-25 14:41:20 -0600

    Update 53-Assignment_with_a_Returned_Value.js

---
+++ 5-Introduction-to-JavaScript/53-Assignment_with_a_Returned_Value.js
@@ -0,0 +1,19 @@
+<!-- add a processed function with the process argument with a numerical value which will return a value. -->
+// Example
+var changed = 0;
+
+function change(num) {
+  return (num + 5) / 3;
+}
+
+changed = change(10);
+
+// Setup
+var processed = 0;
+
+function processArg(num) {
+  return (num + 3) / 5;
+}
+
+// Only change code below this line
+processed = processArg(7);
