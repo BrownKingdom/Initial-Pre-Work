86d3eea5bb6627350905ab5c80f5a0575412389e
BrownKingdom <35633631+BrownKingdom@users.noreply.github.com>
2019-03-24 21:37:46 -0600

    Update 26-Concatenating_Strings_with_the_Plus_Equals_Operator.js

---
+++ 5-Introduction-to-JavaScript/26-Concatenating_Strings_with_the_Plus_Equals_Operator.js
@@ -0,0 +1,9 @@
+<!-- Remember to add spaces. Making strings using the += to add additional string. Remember That the strings go into ''-->
+// Example
+var ourStr = "I come first. ";
+ourStr += "I come second.";
+
+// Only change code below this line
+
+var myStr = 'This is the first sentence. ';
+myStr += 'This is the second sentence.'
