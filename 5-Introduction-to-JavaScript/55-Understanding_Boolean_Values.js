5d90a8d3f98cb43c3d45c394941b3dcecb4e8063
BrownKingdom <35633631+BrownKingdom@users.noreply.github.com>
2019-03-25 14:55:31 -0600

    Update 55-Understanding_Boolean_Values.js

---
+++ 5-Introduction-to-JavaScript/55-Understanding_Boolean_Values.js
@@ -0,0 +1,9 @@
+<!-- Booleans render only a true of false; booleans states are mutually exclusive.  Boolean values are not ever written with quotation marks. -->
+function welcomeToBooleans() {
+
+// Only change code below this line.
+
+return true; // Change this line
+
+// Only change code above this line.
+}
