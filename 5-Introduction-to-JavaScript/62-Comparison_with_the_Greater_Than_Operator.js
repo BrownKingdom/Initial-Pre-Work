0f0e320119e8393fff7f05267bfe296ce1ef8d7b
BrownKingdom <35633631+BrownKingdom@users.noreply.github.com>
2019-03-25 15:54:42 -0600

    Update 62-Comparison_with_the_Greater_Than_Operator.js

---
+++ 5-Introduction-to-JavaScript/62-Comparison_with_the_Greater_Than_Operator.js
@@ -0,0 +1,15 @@
+<!-- This is like the equality except comparing values that are greater than. -->
+function testGreaterThan(val) {
+  if (val > 100) {  // Change this line
+    return "Over 100";
+  }
+  
+  if (val > 10) {  // Change this line
+    return "Over 10";
+  }
+
+  return "10 or Under";
+}
+
+// Change this value to test
+testGreaterThan(10);
