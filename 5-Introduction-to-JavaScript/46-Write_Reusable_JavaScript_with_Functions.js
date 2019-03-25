2ba4e465d2a8d93a6510424b15975e38589a3feb
BrownKingdom <35633631+BrownKingdom@users.noreply.github.com>
2019-03-25 09:41:06 -0600

    Update 46-Write_Reusable_JavaScript_with_Functions.js

---
+++ 5-Introduction-to-JavaScript/46-Write_Reusable_JavaScript_with_Functions.js
@@ -0,0 +1,14 @@
+<!-- dividing up code into reuseable parts with the function -->
+// Example
+function ourReusableFunction() {
+  console.log("Heyya, World");
+}
+
+ourReusableFunction();
+
+// Only change code below this line
+function reusableFunction(){
+    console.log("Hi World");
+}
+
+reusableFunction();
