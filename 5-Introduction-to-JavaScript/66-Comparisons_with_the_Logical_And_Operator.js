8ea2df0485cacba00d0b9e060a552c4423d9aed9
BrownKingdom <35633631+BrownKingdom@users.noreply.github.com>
2019-03-25 16:08:32 -0600

    Update 66-Comparisons_with_the_Logical_And_Operator.js

---
+++ 5-Introduction-to-JavaScript/66-Comparisons_with_the_Logical_And_Operator.js
@@ -0,0 +1,15 @@
+<!-- Compare a value the logical and operator to determine if the operation order is true left to right. -->
+function testLogicalAnd(val) {
+  // Only change code below this line
+
+  if (val <= 50 && val >= 25) {
+      return "Yes";
+    }
+  
+
+  // Only change code above this line
+  return "No";
+}
+
+// Change this value to test
+testLogicalAnd(30);
