d32cdac4d92c07a6af78969ed7e781127f795062
BrownKingdom <35633631+BrownKingdom@users.noreply.github.com>
2019-03-25 07:28:34 -0600

    Update 38-Access_Array_Data_with_Indexes.js

---
+++ 5-Introduction-to-JavaScript/38-Access_Array_Data_with_Indexes.js
@@ -0,0 +1,11 @@
+<!-- Array indexes us the bracket notation like strings, but they specify an entry rather than a character. brackets and commas -->
+// Example
+var ourArray = [50,60,70];
+var ourData = ourArray[0]; // equals 50
+
+// Setup
+var myArray = [50,60,70];
+
+// Only change code below this line.
+var myArray = [20,30,40];
+var myData = myArray[0]; // equals 20
