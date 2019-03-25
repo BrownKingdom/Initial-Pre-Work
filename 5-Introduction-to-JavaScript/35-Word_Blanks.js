406412832eec392406e0237641351fae06febd51
BrownKingdom <35633631+BrownKingdom@users.noreply.github.com>
2019-03-25 07:09:48 -0600

    Update 35-Word_Blanks.js

---
+++ 5-Introduction-to-JavaScript/35-Word_Blanks.js
@@ -0,0 +1,11 @@
+<!-- Use the var sentence to fill word blanks; myNoun, myAdjective, myVerb, or myAdverb. remember to add spaces inside the quotation marks -->
+function wordBlanks(myNoun, myAdjective, myVerb, myAdverb) {
+  // Your code below this line
+  var result = "My " + myAdjective + " " + myNoun + " " + myVerb + " " + myAdverb + ".";
+
+  // Your code above this line
+  return result;
+}
+
+// Change the words here to test your function
+wordBlanks("dog", "big", "ran", "quickly");
