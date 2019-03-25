ab423c5e5aa008dded7fe6a9983d8418547aebaf
BrownKingdom <35633631+BrownKingdom@users.noreply.github.com>
2019-03-25 07:16:08 -0600

    Update 36-Store_Multiple_Values_in_one_Variable_using_JavaScript_Arrays.js

---
+++ 5-Introduction-to-JavaScript/36-Store_Multiple_Values_in_one_Variable_using_JavaScript_Arrays.js
@@ -0,0 +1,6 @@
+<!-- Array variable allow us to store several pieces of data in one place, starting with an open bracket and using commas to separate the using a closing bracket. -->
+// Example
+var ourArray = ["John", 23];
+
+// Only change code below this line.
+var myArray = ["Benjamin", 13];
