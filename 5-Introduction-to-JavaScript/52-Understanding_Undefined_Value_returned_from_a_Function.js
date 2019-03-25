1b397dfdb0643b382e9736a959feaa194306ea53
BrownKingdom <35633631+BrownKingdom@users.noreply.github.com>
2019-03-25 14:33:09 -0600

    Update 52-Understanding_Undefined_Value_returned_from_a_Function.js

---
+++ 5-Introduction-to-JavaScript/52-Understanding_Undefined_Value_returned_from_a_Function.js
@@ -0,0 +1,16 @@
+<!-- A function does not have to include a return, the function processes the inner code as undefined. Remember to place a numeral in the sum equation. -->
+// Example
+var sum = 0;
+function addThree() {
+  sum = sum + 3;
+}
+
+// Only change code below this line
+var sum = 8;
+function addFive() {
+  sum = sum + 5;
+}
+var returnedValue = addFive(5); 
+
+// Only change code above this line
+var returnedValue = addFive();
