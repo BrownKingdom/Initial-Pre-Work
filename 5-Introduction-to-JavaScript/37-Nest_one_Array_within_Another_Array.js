fbe4a2557d7bed8dd94518a2d1d36816dcef066b
BrownKingdom <35633631+BrownKingdom@users.noreply.github.com>
2019-03-25 07:21:16 -0600

    Update 37-Nest_one_Array_within_Another_Array.js

---
+++ 5-Introduction-to-JavaScript/37-Nest_one_Array_within_Another_Array.js
@@ -0,0 +1,6 @@
+<!-- Nest an array inside another array using the bracket notation inside a bracket notation separating the bracket notations with a comma.  remember to use the quotation marks and commas. -->
+// Example
+var ourArray = [["the universe", 42], ["everything", 101010]];
+
+// Only change code below this line.
+var myArray = [["Ezekiel", 13], ["Denver Outlaws", 13]];
