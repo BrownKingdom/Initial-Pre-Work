3872d3182bbf09300049cfc8213f5b2b508665ba
BrownKingdom <35633631+BrownKingdom@users.noreply.github.com>
2019-03-24 20:29:37 -0600

    Update 25-Concatenating_Strings_with_Plus_Operator.js

---
+++ 5-Introduction-to-JavaScript/25-Concatenating_Strings_with_Plus_Operator.js
@@ -0,0 +1,8 @@
+<!-- connecting a string out of other strings by linking them.  Remember to add a space after the second single quote. -->
+// Example
+var ourStr = "I come first. " + "I come second.";
+
+// Only change code below this line
+
+var myStr = 'This is the start.' + ' This is the end.';
+
