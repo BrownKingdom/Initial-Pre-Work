760aa75deeb6d8560f317d2a5da06615ecd919b4
BrownKingdom <35633631+BrownKingdom@users.noreply.github.com>
2019-03-25 14:16:06 -0600

    Update 51-Return_a_Value_from_a_Function_with_Return.js

---
+++ 5-Introduction-to-JavaScript/51-Return_a_Value_from_a_Function_with_Return.js
@@ -0,0 +1,16 @@
+<!-- We are able to use a return statement to send a value back out of a function just as we can pass values into a function with arguments. -->
+// Example
+function minusSeven(num) {
+  return num - 7;
+}
+
+// Only change code below this line
+function timesFive(num) {
+  return num * 5;
+}
+
+var answer = timesFive(5);
+var answer = timesFive(2);
+var answer = timesFive(0);
+
+console.log(minusSeven(10));
