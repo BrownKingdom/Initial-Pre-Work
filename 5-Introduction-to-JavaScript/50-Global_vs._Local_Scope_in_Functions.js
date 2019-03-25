5138b2df2d943a323d84260606f9ea103484e572
BrownKingdom <35633631+BrownKingdom@users.noreply.github.com>
2019-03-25 12:57:24 -0600

    Update 50-Global_vs._Local_Scope_in_Functions.js

---
+++ 5-Introduction-to-JavaScript/50-Global_vs._Local_Scope_in_Functions.js
@@ -0,0 +1,14 @@
+<!-- When you have include both a local and global function the local will have precedence over global variable. -->
+// Setup
+var outerWear = "T-Shirt";
+
+function myOutfit() {
+  // Only change code below this line
+  var outerWear = "sweater";
+  
+  
+  // Only change code above this line
+  return outerWear;
+}
+
+myOutfit();
