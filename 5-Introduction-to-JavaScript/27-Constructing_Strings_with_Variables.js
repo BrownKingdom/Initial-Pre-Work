ad64cd319e840e4b3eeec3eb4e6144a05dcabb30
BrownKingdom <35633631+BrownKingdom@users.noreply.github.com>
2019-03-24 21:36:49 -0600

    Update 27-Constructing_Strings_with_Variables.js

---
+++ 5-Introduction-to-JavaScript/27-Constructing_Strings_with_Variables.js
@@ -0,0 +1,8 @@
+<!-- Set strings equal to your name and build between the strings with two + -->
+// Example
+var ourName = "freeCodeCamp";
+var ourStr = "Hello, our name is " + ourName + ", how are you?";
+
+// Only change code below this line
+var myName = "Yolanda Honeycutt-Brown";
+var myStr = "My name is" + myName + " and I am well!";
