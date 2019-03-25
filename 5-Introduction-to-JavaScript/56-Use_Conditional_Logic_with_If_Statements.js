464d8d642081e3df24ec803e28e28542eb8a6672
BrownKingdom <35633631+BrownKingdom@users.noreply.github.com>
2019-03-25 15:12:58 -0600

    Update 56-Use_Conditional_Logic_with_If_Statements.js

---
+++ 5-Introduction-to-JavaScript/56-Use_Conditional_Logic_with_If_Statements.js
@@ -0,0 +1,24 @@
+<!-- When a boolean condition is render true the true statement will be executed inside the curly brackets. Entering a if statement to check your true false.-->
+// Example
+function ourTrueOrFalse(isItTrue) {
+  if (isItTrue) { 
+    return "Yes, it's true";
+  }
+  return "No, it's false";
+}
+
+// Setup
+function trueOrFalse(wasThatTrue) {
+
+  // Only change code below this line.
+if (wasThatTrue) {
+    return "Yes, that was true";
+}
+  return "No, that was false";
+  
+  // Only change code above this line.
+
+}
+
+// Change this value to test
+trueOrFalse(true);
