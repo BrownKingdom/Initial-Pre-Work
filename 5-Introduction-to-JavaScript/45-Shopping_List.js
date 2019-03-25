e8c7e22dfead3f66e3fe35a45908dfae122e8c2b
BrownKingdom <35633631+BrownKingdom@users.noreply.github.com>
2019-03-25 09:21:24 -0600

    Update 45-Shopping_List.js

---
+++ 5-Introduction-to-JavaScript/45-Shopping_List.js
@@ -0,0 +1,9 @@
+<!-- creating a list with a string of names of items. Remember to set brackets inside brackets and use commas. -->
+var myList = [
+             ["Carrots", 10],
+             ["Avocados", 3],
+             ["Mangoes", 3],
+             ["Pineapples", 2],
+             ["Coconut Oil", 1],
+];
+
