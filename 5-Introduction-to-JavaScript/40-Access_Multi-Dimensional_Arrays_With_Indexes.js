31e58a7467314fda5fad9743ad58c6f8e3c248db
BrownKingdom <35633631+BrownKingdom@users.noreply.github.com>
2019-03-25 08:30:00 -0600

    Update 40-Access_Multi-Dimensional_Arrays_With_Indexes.js

---
+++ 5-Introduction-to-JavaScript/40-Access_Multi-Dimensional_Arrays_With_Indexes.js
@@ -0,0 +1,6 @@
+<!-- Accessing an array inside an array with bracket notation inside a bracket notation using the arr field. -->
+// Setup
+var myArray = [[1,2,3], [4,5,6], [7,8,9], [[10,11,12], 13, 14]];
+
+// Only change code below this line.
+var myData = myArray[2][1];
