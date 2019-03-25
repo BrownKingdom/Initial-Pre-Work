22d7d28b9ee7f26d0aec9afa803f6c2e503ec0e3
BrownKingdom <35633631+BrownKingdom@users.noreply.github.com>
2019-03-25 06:51:44 -0600

    Update 32-Use_Bracket_Notation_to_Find_the_Nth_Character_in_a_String.js

---
+++ 5-Introduction-to-JavaScript/32-Use_Bracket_Notation_to_Find_the_Nth_Character_in_a_String.js
@@ -0,0 +1,11 @@
+<!-- using bracket notation to target a different character with in string remember to count up from 0 -->
+// Example
+var firstName = "Ada";
+var secondLetterOfFirstName = firstName[1];
+
+// Setup
+var lastName = "Lovelace";
+
+// Only change code below this line.
+var thirdLetterOfLastName = lastName[2];
+
