9ef54d9e5e70e402e7a5729085e7350fd3b58395
BrownKingdom <35633631+BrownKingdom@users.noreply.github.com>
2019-03-25 15:23:09 -0600

    Update 58-Comparison_with_the_Strict_Equality_Operator.js

---
+++ 5-Introduction-to-JavaScript/58-Comparison_with_the_Strict_Equality_Operator.js
@@ -0,0 +1,11 @@
+<!-- Use the strict equality triple equal in the if statement so the function will render a equal when the numerical val is strictly equal to that numerical value. -->
+// Setup
+function testStrict(val) {
+  if (val === 7) { // Change this line
+    return "Equal";
+  }
+  return "Not Equal";
+}
+
+// Change this value to test
+testStrict(10);
