ff3cfd707b4048872ba695cd500e71244b08cbd3
BrownKingdom <35633631+BrownKingdom@users.noreply.github.com>
2019-03-25 09:48:17 -0600

    Update 47-Passing_Values_to_Functions_with_Arguments.js

---
+++ 5-Introduction-to-JavaScript/47-Passing_Values_to_Functions_with_Arguments.js
@@ -0,0 +1,12 @@
+<!-- Using the parameters feature set placeholders for values to be entered into a function. Values entered "passed" are called arguments. -->
+// Example
+function ourFunctionWithArgs(a, b) {
+  console.log(a - b);
+}
+ourFunctionWithArgs(10, 5); // Outputs 5
+
+// Only change code below this line.
+function functionWithArgs(a,b) {
+  console.log(a + b);
+}
+functionWithArgs(7, 9);
