b9502c17f062e00b62b73027a338b5691121713d
BrownKingdom <35633631+BrownKingdom@users.noreply.github.com>
2019-03-25 22:13:38 -0600

    Update 90-Accessing_Nested_Objects.js

---
+++ 5-Introduction-to-JavaScript/90-Accessing_Nested_Objects.js
@@ -0,0 +1,15 @@
+<!-- sub-properties access by chaining together dot or bracket notations. -->
+// Setup
+var myStorage = {
+  "car": {
+    "inside": {
+      "glove box": "maps",
+      "passenger seat": "crumbs"
+     },
+    "outside": {
+      "trunk": "jack"
+    }
+  }
+};
+
+var gloveBoxContents = myStorage.car.inside["glove box"]; // Change this line
