88118ff36bc2f7e5dc27bae51bb87f3611f77100
BrownKingdom <35633631+BrownKingdom@users.noreply.github.com>
2019-03-26 15:08:34 -0600

    Update 20-Use-class-Syntax-to-Define-a-Constructor-Function.js

---
+++ 6-Introduction-to-the-ES6-Challenges/20-Use-class-Syntax-to-Define-a-Constructor-Function.js
@@ -0,0 +1,10 @@
+<!-- adding support for defining object literal. -->
+const createPerson = (name, age, gender) => {
+  "use strict";
+  // change code below this line
+ 
+const createPerson = (name, age, gender) => ( { name, age, gender });
+
+  // change code above this line
+};
+console.log(createPerson("Zodiac Hasbro", 56, "male")); // returns a proper object
