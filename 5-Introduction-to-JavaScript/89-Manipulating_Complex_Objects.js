f6367df2e59c9ea728ba7a04eada06834302bd6b
BrownKingdom <35633631+BrownKingdom@users.noreply.github.com>
2019-03-25 22:03:44 -0600

    Update 89-Manipulating_Complex_Objects.js

---
+++ 5-Introduction-to-JavaScript/89-Manipulating_Complex_Objects.js
@@ -0,0 +1,25 @@
+<!-- Storing data in a flexible data structure allow me to make arbitary combinations of strings ect. -->
+var myMusic = [
+  {
+    "artist": "Billy Joel",
+    "title": "Piano Man",
+    "release_year": 1973,
+    "formats": [ 
+      "CD",
+      "8T",
+      "LP"
+    ],
+    "gold": true
+  },
+  // Add record here
+{
+  "artist": "Bob",
+  "title": "Hello",
+  "release_year": 1950,
+  "formats": [
+    "CD",
+    "8T"
+  ]
+
+}
+];
