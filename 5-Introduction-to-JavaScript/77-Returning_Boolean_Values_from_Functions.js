87f36541d964ab75c0d663868b029e55f82b761f
BrownKingdom <35633631+BrownKingdom@users.noreply.github.com>
2019-03-25 18:32:23 -0600

    Update 77-Returning_Boolean_Values_from_Functions.js

---
+++ 5-Introduction-to-JavaScript/77-Returning_Boolean_Values_from_Functions.js
@@ -0,0 +1,8 @@
+<!-- Returning a true false value with the comparison operators to produce boolean values. -->
+function isLess(a, b) {
+  // Fix this code
+  return (a < b) ;
+}
+
+// Change these values to test
+isLess(10, 15);
