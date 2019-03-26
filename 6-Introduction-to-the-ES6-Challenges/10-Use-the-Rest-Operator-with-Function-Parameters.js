cf57a82d690b80d22c5432cfbe7b83bdd3832d24
BrownKingdom <35633631+BrownKingdom@users.noreply.github.com>
2019-03-26 11:41:35 -0600

    Update 10-Use-the-Rest-Operator-with-Function-Parameters.js

--- 6-Introduction-to-the-ES6-Challenges/10-Use-the-Rest-Operator-with-Function-Parameters.js
+++ 6-Introduction-to-the-ES6-Challenges/10-Use-the-Rest-Operator-with-Function-Parameters.js
@@ -3,3 +3,10 @@ function sum(...args) {
   return args.reduce((a,b) => a + b, 0);
 }
 console.log(sum(1, 2, 3)); // 6
+
+
+<!--  I also use the array function but it would not complete. -->
+  function sum(...array) {
+  return array.reduce((a,b) => a + b, 0);
+}
+console.log(sum(1, 2, 3)); // 6
