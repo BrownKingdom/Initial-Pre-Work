a4c5aa2e1b73b7e45facb85f87ca7bc59b7909d7
BrownKingdom <35633631+BrownKingdom@users.noreply.github.com>
2019-03-26 00:28:55 -0600

    Update 101-Generate_Random_Fractions_with_JavaScript.js

---
+++ 5-Introduction-to-JavaScript/101-Generate_Random_Fractions_with_JavaScript.js
@@ -0,0 +1,9 @@
+<!-- math random generates Randomized numbers are useful for creating randomized behaviors with decimals. -->
+function randomFraction() {
+
+  // Only change code below this line.
+
+  return Math.random ();
+
+  // Only change code above this line.
+}
