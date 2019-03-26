363584bae374640aa4578d7e1ca713249690d5ce
BrownKingdom <35633631+BrownKingdom@users.noreply.github.com>
2019-03-25 23:24:22 -0600

    Update 94-Iterate_with_JavaScript_For_Loops.js

---
+++ 5-Introduction-to-JavaScript/94-Iterate_with_JavaScript_For_Loops.js
@@ -0,0 +1,15 @@
+<!-- use the loop to run through code multiple times for a specified number of time. -->
+// Example
+var ourArray = [];
+
+for (var i = 0; i < 5; i++) {
+  ourArray.push(i);
+}
+
+// Setup
+var myArray = [];
+
+// Only change code below this line.
+for (var i = 1; i < 6; i++) {
+  myArray.push(i);
+}
