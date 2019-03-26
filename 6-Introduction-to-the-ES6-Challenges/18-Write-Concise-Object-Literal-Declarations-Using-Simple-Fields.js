4046b3c91c832cfc0cac8c583865669d2889ecb1
BrownKingdom <35633631+BrownKingdom@users.noreply.github.com>
2019-03-26 13:38:26 -0600

    Update 18-Write-Concise-Object-Literal-Declarations-Using-Simple-Fields.js

---
+++ 6-Introduction-to-the-ES6-Challenges/18-Write-Concise-Object-Literal-Declarations-Using-Simple-Fields.js
@@ -0,0 +1,10 @@
+const createPerson = (name, age, gender) => {
+  "use strict";
+  // change code below this line
+    const creatPerson = (name, age, gender) => {
+    return {name,age,gender};
+  };  
+  // change code above this line
+};
+console.log(createPerson("Zodiac Hasbro", 56, "male")); // returns a proper object
+<!-- simplify -->
