3410b7e11ec4adb28c239d71a555055f99b8ef4c
BrownKingdom <35633631+BrownKingdom@users.noreply.github.com>
2019-03-26 00:59:19 -0600

    Update 106-Use_the_Conditional_(Ternary)_Operator.js

---
+++ 5-Introduction-to-JavaScript/106-Use_the_Conditional_(Ternary)_Operator.js
@@ -0,0 +1,6 @@
+<!-- Ternary and Conditional operators are the same and can be used as a one line if-else expression. -->
+function checkEqual(a, b) {
+  return a === b ? true : false;
+}
+
+checkEqual(1, 2);
