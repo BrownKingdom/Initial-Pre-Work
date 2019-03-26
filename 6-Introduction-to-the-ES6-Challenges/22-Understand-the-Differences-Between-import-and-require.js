7ee30cf1bc74848e0d013ab8108436a92cd68138
BrownKingdom <35633631+BrownKingdom@users.noreply.github.com>
2019-03-26 16:07:49 -0600

    Update 22-Understand-the-Differences-Between-import-and-require.js

---
+++ 6-Introduction-to-the-ES6-Challenges/22-Understand-the-Differences-Between-import-and-require.js
@@ -0,0 +1,4 @@
+"use strict";
+import { capitalizeString } from "string_functions"
+capitalizeString("hello!");
+<!-- this handles non browser features does not require the extra coding that required did. -->
