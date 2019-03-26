532940607019825b889cf28fd667cc8d8b2e9f8a
BrownKingdom <35633631+BrownKingdom@users.noreply.github.com>
2019-03-25 21:14:31 -0600

    Update 80-Build_JavaScript_Objects.js

---
+++ 5-Introduction-to-JavaScript/80-Build_JavaScript_Objects.js
@@ -0,0 +1,17 @@
+<!-- Objects are similar to arrays, objects use indexes to access and modify their data, which data objects that are called properties. -->
+// Example
+var ourDog = {
+  "name": "Camper",
+  "legs": 4,
+  "tails": 1,
+  "friends": ["everything!"]
+};
+
+// Only change code below this line.
+
+var myDog = {
+  "name": "string",
+  "legs": 4,
+  "tails": 1,
+  "friends": ["hooray!"]
+};
