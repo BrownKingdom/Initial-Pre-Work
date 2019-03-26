d1962bcc8a3159ebeabcccdf95c9d97985c7df67
BrownKingdom <35633631+BrownKingdom@users.noreply.github.com>
2019-03-25 23:02:53 -0600

    Update 93-Iterate_with_JavaScript_While_Loops.js

---
+++ 5-Introduction-to-JavaScript/93-Iterate_with_JavaScript_While_Loops.js
@@ -0,0 +1,11 @@
+<!-- running a while loop, this runs as long as conditions are true and stops when it is not. -->
+// Setup
+var myArray = [];
+
+// Only change code below this line.
+var myArray = [];
+var i = 0;
+while (i < 5) {
+    myArray.push(i);
+    i++;
+}
