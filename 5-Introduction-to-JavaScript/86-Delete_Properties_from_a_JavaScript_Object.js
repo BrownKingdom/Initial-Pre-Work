059b28113e9a31b954e81380089c1223f331cd64
BrownKingdom <35633631+BrownKingdom@users.noreply.github.com>
2019-03-25 21:39:15 -0600

    Update 86-Delete_Properties_from_a_JavaScript_Object.js

---
+++ 5-Introduction-to-JavaScript/86-Delete_Properties_from_a_JavaScript_Object.js
@@ -0,0 +1,23 @@
+<!-- use the delete property to change code without removing it from a field. -->
+// Example
+var ourDog = {
+  "name": "Camper",
+  "legs": 4,
+  "tails": 1,
+  "friends": ["everything!"],
+  "bark": "bow-wow"
+};
+
+delete ourDog.bark;
+
+// Setup
+var myDog = {
+  "name": "Happy Coder",
+  "legs": 4,
+  "tails": 1,
+  "friends": ["freeCodeCamp Campers"],
+  "bark": "woof"
+};
+
+// Only change code below this line.
+delete myDog.tails;
