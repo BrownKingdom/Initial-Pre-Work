29e1fb03094b0189ce47b0858987dd3613b41c4c
BrownKingdom <35633631+BrownKingdom@users.noreply.github.com>
2019-03-26 09:21:19 -0600

    Update 3-Declare-a-Read-Only-Variable-with-the-const-Keyword.js

---
+++ 6-Introduction-to-the-ES6-Challenges/3-Declare-a-Read-Only-Variable-with-the-const-Keyword.js
@@ -0,0 +1,15 @@
+<!-- Let allows me to use const keywords, these declarations are read-only and cannot be reassigned. -->
+function printManyTimes(str) {
+  "use strict";
+
+  // change code below this line
+
+  const SENTENCE = str + " is cool!";
+  for(let i = 0; i < str.length; i+=2) {
+    console.log(SENTENCE);
+  }
+
+  // change code above this line
+
+}
+printManyTimes("freeCodeCamp");
